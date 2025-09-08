fc0688963f3cebf3e3f512477802b5096b285859
phamviethoa <phamviethoa2416@gmail.com>
2025-09-09 00:01:29 +0700

    fix: create Category model

---
+++ src/main/java/com/example/model/Category.java
@@ -0,0 +1,34 @@
+package com.example.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class Category {
+    private String categoryId;
+    private String name;
+    private String description;
+    private boolean isActive;
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
+
+    @Override
+    public String toString() {
+        return "Category{" +
+                "categoryId=" + categoryId +
+                ", name='" + name + '\'' +
+                ", description='" + description + '\'' +
+                ", isActive=" + isActive +
+                ", createdAt=" + createdAt +
+                ", updatedAt=" + updatedAt +
+                '}';
+    }
+}
